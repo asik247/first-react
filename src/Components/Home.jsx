eebd9f237e5727ffc70a5bfdb3d620bc464e8a33
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-23 23:52:57 +0600

    tailwind added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Hello iam react first code here now</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est, repellat commodi sint sed nam iste dolorem eligendi eum enim, nemo quaerat excepturi ipsum laudantium, voluptatem sit modi ea aut.</p>
+        <div >
+            <h1 className='border-2 border-green-500 text-center'>Hello iam react first code here now</h1>
+            <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est, repellat commodi sint sed nam iste dolorem eligendi eum enim, nemo quaerat excepturi ipsum laudantium, voluptatem sit modi ea aut.</p>
         </div>
     );
 };
