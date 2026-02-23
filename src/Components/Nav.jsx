938cba9f99b10cc4452055472b8085c82680bdb0
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-24 00:29:03 +0600

    Card added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -46,7 +46,7 @@ const Nav = () => {
 
             </div>
 
-            <div className="hero bg-base-200 min-h-screen">
+            <div className="hero bg-base-200 min-h-screen mb-30">
                 <div className="hero-content flex-col lg:flex-row">
                     <img
                         src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
@@ -61,6 +61,98 @@ const Nav = () => {
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
+            </div>
+            <div className='grid grid-cols-3 gap-4 w-11/12 mx-auto'>
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        Card Title
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        Card Title
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        Card Title
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        Card Title
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        Card Title
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
+                    </div>
+                </div>
+            </div>
             </div>
         </>
 
