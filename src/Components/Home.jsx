920d8f4c7ce63734559aee24419cd1ea5200dddf
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-23 23:02:17 +0600

    Home file added

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello iam react first code here now</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est, repellat commodi sint sed nam iste dolorem eligendi eum enim, nemo quaerat excepturi ipsum laudantium, voluptatem sit modi ea aut.</p>
+        </div>
+    );
+};
+
+export default Home;
