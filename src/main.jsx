ef0b977e870820c2e18921f9965680fb25975db7
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-24 00:22:16 +0600

    nav added

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import Home from './Components/home.jsx'
+import App from './App.jsx'
+import Nav from './Components/Nav.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
-  <Home></Home>
+  {/* <Home></Home> */}
+  {/* <App></App> */}
+  <Nav></Nav>
   </StrictMode>,
 )
