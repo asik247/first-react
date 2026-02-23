50fe93647209b87603764eedfa76f27ad7a39f97
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-23 23:59:51 +0600

    y

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,22 @@ const Home = () => {
         <div >
             <h1 className='border-2 border-green-500 text-center'>Hello iam react first code here now</h1>
             <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est, repellat commodi sint sed nam iste dolorem eligendi eum enim, nemo quaerat excepturi ipsum laudantium, voluptatem sit modi ea aut.</p>
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary text-gray-500">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
